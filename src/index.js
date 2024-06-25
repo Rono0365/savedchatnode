ee2cc89397a5489e94f2d11c87e96a8cae5f2510
Rono <63807776+Rono0365@users.noreply.github.com>
2024-06-25 06:05:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,12 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.set('view engine', 'ejs');
-app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Configure multer to handle file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
