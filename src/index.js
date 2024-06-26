df21a03ea2d66f92093509a50c66447c8d77a238
Rono <63807776+Rono0365@users.noreply.github.com>
2024-06-26 05:03:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,8 +23,9 @@ const corsOptions = {
 app.set('view engine', 'ejs');
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 })
 // Configure multer to handle file uploads
